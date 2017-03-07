ebcf0fbc6b7d215c9c85c117fbb18fb46806e2ef
Jonathan Armond <jon.armond@gmail.com>
2017-03-07 07:22:10 +0000

    Add show and error instances to LocoError

--- src/Loco/Error.hs
+++ src/Loco/Error.hs
@@ -5,8 +5,16 @@ import Control.Monad.Except
 
 type LocoEval  = Either LocoError
 data LocoError = ArgError Int Int
-               | TypeError String String
+               | TypeError String
                | ParserError String
                | UnknownError String
-               deriving Show
+instance Show LocoError where show = showError
+instance Error LocoError where
+  noMsg = UnknownError "unknown error"
+  strMsg = UnknownError
 
+showError :: LocoError -> String
+showError (ArgError n m) = "expected " ++ show n ++ " args but found " ++ show m
+showError (TypeError msg) = "invalid types: " ++ msg
+showError (ParserError msg) = "parse error: " ++ msg
+showError (UnknownError msg) = msg
