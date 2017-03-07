5779119660430a11fa0e705902763df21af6a9f7
Jonathan Armond <jon.armond@gmail.com>
2017-03-07 12:56:23 +0000

    Switch to throwError

--- src/Loco/Eval.hs
+++ src/Loco/Eval.hs
@@ -5,7 +5,7 @@ import Loco.Error
 import Loco.AST
 
 import Data.IORef
-import Control.Exception (throw)
+import Control.Monad.Except (throwError)
 import Control.Monad
 
 
@@ -17,15 +17,15 @@ evalLine (CommandLine linum st) = evalSt st
 evalSt :: Statement -> LocoEval LocoValue
 evalSt (Command cmd args) = undefined
 evalSt (Dim (Variable name t) args) = undefined
-evalSt (Dim _ _) = throw $ TypeError "expected variable for DIM"
+evalSt (Dim _ _) = throwError $ TypeError "expected variable for DIM"
 evalSt (For (Variable name t) from to step) = undefined
-evalSt (For _ _ _ _) = throw $ TypeError "expected variable for FOR"
+evalSt (For _ _ _ _) = throwError $ TypeError "expected variable for FOR"
 evalSt (If expr@(BoolBinary _ _ _) thenSt elseSt) = undefined
-evalSt (If _ _ _) = throw $ TypeError "expected boolean expression for IF"
+evalSt (If _ _ _) = throwError $ TypeError "expected boolean expression for IF"
 evalSt (While expr@(BoolBinary _ _ _)) = undefined
-evalSt (While _) = throw $ TypeError "expected boolean expression for WHILE"
+evalSt (While _) = throwError $ TypeError "expected boolean expression for WHILE"
 evalSt (Assign (Variable name t) expr) = undefined
-evalSt (Assign _ _) = throw $ TypeError "expected variable for assignment"
+evalSt (Assign _ _) = throwError $ TypeError "expected variable for assignment"
 
 -- |Evaluate an expression.
 eval :: LocoExpr -> LocoEval LocoValue
@@ -41,20 +41,20 @@ aeval Divide a b   = join $ locoDiv <$> (eval a) <*> (eval b)
 locoAdd :: LocoValue -> LocoValue -> LocoEval LocoValue
 locoAdd (Int a) (Int b) = return $ Int (a + b)
 locoAdd (Real a) (Real b) = return $ Real (a + b)
-locoAdd a b = throw $ TypeError (show a ++ " " ++ show b)
+locoAdd a b = throwError $ TypeError (show a ++ " " ++ show b)
 
 locoSub :: LocoValue -> LocoValue -> LocoEval LocoValue
 locoSub (Int a) (Int b) = return $ Int (a - b)
 locoSub (Real a) (Real b) = return $ Real (a - b)
-locoSub a b = throw $ TypeError (show a ++ " " ++ show b)
+locoSub a b = throwError $ TypeError (show a ++ " " ++ show b)
 
 locoMul :: LocoValue -> LocoValue -> LocoEval LocoValue
 locoMul (Int a) (Int b) = return $ Int (a * b)
 locoMul (Real a) (Real b) = return $ Real (a * b)
-locoMul a b = throw $ TypeError (show a ++ " " ++ show b)
+locoMul a b = throwError $ TypeError (show a ++ " " ++ show b)
 
 locoDiv :: LocoValue -> LocoValue -> LocoEval LocoValue
 locoDiv (Int a) (Int b) = return $ Int (a `div` b)
 locoDiv (Real a) (Real b) = return $ Real (a / b)
-locoDiv a b = throw $ TypeError (show a ++ " " ++ show b)
+locoDiv a b = throwError $ TypeError (show a ++ " " ++ show b)
 
