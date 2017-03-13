36e546dbc17a0a06eabc28a8d40eac4fde7e8e7d
Jonathan Armond <jon.armond@gmail.com>
2017-03-13 14:37:02 +0000

    Clear out commented code

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import Loco.AST
 import Loco.Parser
 import Loco.Error
 import Loco.Eval
+import Loco.Interpreter
 
 import System.IO
 import System.Environment (getArgs)
@@ -15,14 +16,8 @@ main = do
   args <- getArgs
   if null args then repl else runParseStatement $ args
 
-  -- case runParseLine "10 PRINT 3" of
-  --        Left err -> putStrLn err
-  --        Right (CommandLine n line) -> do
-  --          putStrLn "Successfully parsed:"
-  --          putStrLn $ show n ++ " " ++ show line
-
-readPrompt :: String -> IO String
-readPrompt prompt = putStr prompt >> hFlush stdout >> getLine
+readPrompt :: IO String
+readPrompt = prompt >> hFlush stdout >> getLine
 
 readLine :: String -> LocoEval CommandLine
 readLine = runParseLine
